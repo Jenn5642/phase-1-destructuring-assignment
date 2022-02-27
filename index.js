e8b4223c016fc5e351b3ab588e24fc7ab1f1e7c7
Jenn Robbins <Jennrobbins3@gmail.com>
2022-02-26 21:03:38 -0500

    commit tree d9dfb3038d859cadc5b15403317e065e1f73e02b

--- index.js
+++ index.js
@@ -26,7 +26,65 @@ const nestedMuppet = {
 };
 
 // Strings
-
+const sounds = {
+  moo: "cow",
+  neigh: "horse",
+  baa: "sheep",
+  oink: "pig",
+  cluck: "chicken"
+}
+const { moo, neigh, baa, oink, cluck } = sounds;
+const animals = {
+  bessie: "cow",
+  dolly: "sheep",
+  babe: "pig",
+  little: "chicken"
+}
+const {bessie, dolly, babe, little } = animals
+const diffColors = {
+  blackAndWhite: "cow",
+  black: "sheep",
+  pink: "pig"
+}
+const {blackAndWhite, black, pink} = diffColors
+const colorRainbow = {
+  red: 'red',
+  orange: 'orange',
+  yellow: 'yellow',
+  green: 'green',
+  blue: 'blue',
+  indigo: 'indigo',
+  violet: 'violet'
+}
+const {red, orange, yellow, green, blue, indigo, violet} = colorRainbow
+const rainbow = {
+  r: "red",
+  o: "orange",
+  y: "yellow",
+  g: "green",
+  b: "blue",
+  v: "violet"
+}
+const {r, o, y, g, b, v } = rainbow
+const diffPurp = {
+  indg: "indigo"
+}
+const { indg } = diffPurp
+const objectDecon = {
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit"
+}
+const { muppetName, color, song, job, partner } = objectDecon
+const songsToSing = {
+  song2: "Moving Right Along",
+  song4: "I Hope That Something Better Comes Along",
+  nestedJob: "Host of The Muppet Show",
+  nestedPartner: "Miss Piggy"
+}
+const { song2, song4, nestedJob, nestedPartner } = songsToSing
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
